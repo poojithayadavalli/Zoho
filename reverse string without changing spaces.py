c312e59629c920b776488c13e77e5777ba61cfcc
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-21 20:57:56 +0530

    Create reverse string without changing spaces.py

---
+++ reverse string without changing spaces.py
@@ -0,0 +1,14 @@
+"""
+Write a program to Reverse the given string while preserving the position of spaces.
+
+Examples:
+
+Input  : "abc de"
+Output : edc ba
+
+Input : "intern at geeks"
+Output : skeegt an retni
+
+Input : "Help others"
+Output : sreh topleH
+"""
