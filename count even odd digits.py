5d8dd857350b4b0a7f7ff0fa5021f72dc144bf45
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-21 20:48:44 +0530

    Create count even odd digits.py

---
+++ count even odd digits.py
@@ -0,0 +1,25 @@
+"""
+A certain number is given and the task is to count even digits and odd digits of the number and also of even digits are present even number of times and similarly for odd numbers.
+
+Print Yes If:
+   If number contains even digits even number of time
+   Odd digits odd number of times
+Else 
+   Print No
+Examples :
+
+Input : 22233
+Output : NO
+         count_even_digits = 3
+         count_odd_digits = 2
+         In this number even digits occur odd number of times and odd 
+         digits occur even number of times so its print NO.
+
+Input : 44555
+Output : YES
+        count_even_digits = 2
+        count_odd_digits = 3
+        In this number even digits occur even number of times and odd 
+        digits occur odd number of times so its print YES.
+        
+"""
