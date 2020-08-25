456d8ff587c39cf4e21253245847d2647c06bbdc
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-25 14:42:23 +0530

    Create power check.py

---
+++ power check.py
@@ -0,0 +1,25 @@
+"""
+Given two positive numbers x and y, check if y is a power of x or not.
+
+Input:
+First line contains an integer, the number of test cases 'T'. Each test case should contain two positive numbers x and y.
+
+Output:
+Print 1 if y is a power of x, else print 0.
+
+Constraints: 
+1 <= T <= 30
+1 <= x <= 103
+1 <= y <= 108
+
+Example:
+Input:
+2
+2 8
+1 3
+Output:
+1
+0
+
+Explanation: 8 is a power of 2, but 3 is not a power of 1.
+"""
