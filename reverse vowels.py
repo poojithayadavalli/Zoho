51a60c1face1fae0bb826bcc8d34ca03291e6ff6
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-25 14:44:42 +0530

    Create reverse vowels.py

---
+++ reverse vowels.py
@@ -0,0 +1,23 @@
+"""
+Given a string, your task is to reverse the vowels of string.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case contains a string.
+
+Output:
+Print the string with the vowels reversed.
+
+Constraints:
+1<=T<=10^5
+1<=length of the string<=10^5
+
+Example:
+Input:
+2
+hello world
+geeks for geeks
+
+Output:
+hollo werld
+geeks for geeks
+"""
