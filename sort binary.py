59efbf452d2193f41da21646be51d306f8feff3f
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-25 14:50:36 +0530

    Create sort binary.py

---
+++ sort binary.py
@@ -0,0 +1,21 @@
+"""
+Given a binary array A[] of size N. The task is to arrange the array in increasing order.
+
+Note: The binary array contains only 0  and 1.
+
+Constraints:
+1 <= N <= 106
+0 <= A[i] <= 1
+
+Example 1:
+
+Input:
+N = 5
+A[] = {1,0,1,1,0}
+Output: 0 0 1 1 1
+Example 2:
+
+Input:
+N = 10
+A[] = {1,0,1,1,1,1,1,0,0,0}
+Output: 0 0 0 0 1 1 1 1 1 1
