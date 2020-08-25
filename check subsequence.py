35962daff4e46f436de36310b37209c2c972a3ab
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-25 14:27:32 +0530

    Create check subsequence.py

---
+++ check subsequence.py
@@ -0,0 +1,14 @@
+"""
+Given two strings str1 and str2, find if str1 is a subsequence of str2. A subsequence is a sequence that can be derived from another sequence by deleting some elements without changing the order of the remaining elements (source: wiki). Expected time complexity is linear.
+
+Examples :
+
+Input: str1 = "AXY", str2 = "ADXCPY"
+Output: True (str1 is a subsequence of str2)
+
+Input: str1 = "AXY", str2 = "YADXCP"
+Output: False (str1 is not a subsequence of str2)
+
+Input: str1 = "gksrek", str2 = "geeksforgeeks"
+Output: True (str1 is a subsequence of str2)
+"""
