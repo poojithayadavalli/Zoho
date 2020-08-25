bec521b83f16bf484970f36cc3e847c714b8d374
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-25 14:15:42 +0530

    Create all subsets with sum.py

---
+++ all subsets with sum.py
@@ -0,0 +1,18 @@
+Given an array of integers and a sum, the task is to print all subsets of given array with sum equal to given sum.
+
+Input: array{2, 3, 5, 8, 10}
+       sum=10
+Output: {2, 3, 5}
+       {2, 8}
+       {10}
+Input : arr[] = {2, 3, 5, 6, 8, 10}
+        sum = 10
+Output : 5 2 3
+         2 8
+         10
+
+Input : arr[] = {1, 2, 3, 4, 5}
+        sum = 10
+Output : 4 3 2 1 
+         5 3 2 
+         5 4 1
