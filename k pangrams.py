c129aef89ac022847a2ad78d6886e4f0da89b13d
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-27 13:41:14 +0530

    Create k pangrams.py

---
+++ k pangrams.py
@@ -0,0 +1,32 @@
+"""
+Given a String S and an integer k we need to find that whether the string can be changed into Pangram string by performing at most k changes and that changes can be only modification in a string (ie In one change we can remove existing character and add new character).
+
+Input
+first line of input contains of an integer 'T' denoting number od test cases
+first line of each testcase contains String 'S'.
+second line of each testcase contains an integer 'k' denoting the number of changes that are allowed. 
+
+
+Output
+for each case in a new line print 1 for Yes and  0 for No .
+
+Constraints
+1<=T<=40
+1<=S.length()<=10^6
+0<=k<=10^9
+
+Example
+
+Input
+
+2
+qwqqwqeqqwdsdadsdasadsfsdsdsdasasas
+4
+qwqqwqeqqwdsdadsdasadsfsdsdsdasasas
+24
+
+
+Output
+0
+1
+"""
