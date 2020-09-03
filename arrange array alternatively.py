5345ffdf67a77a58f486446fafa6d16d3a23fcd5
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-03 22:02:19 +0530

    Update arrange array alternatively.py

--- arrange array alternatively.py
+++ arrange array alternatively.py
@@ -1,18 +1,24 @@
 """
-Given a sorted array of positive integers. Your task is to rearrange  the array elements alternatively i.e first element should be max value, second should be min value, third should be second max, fourth should be second min and so on...
+Given a sorted array of positive integers. Your task is to rearrange  the array elements alternatively i.e first element should be max value,
+
+second should be min value,third should be second max, fourth should be second min and so on...
 
 Note: O(1) extra space is allowed. Also, try to modify the input array as required.
-
-Input:
-First line of input conatins number of test cases T. First line of test case contain an integer denoting the array size N and second line of test case contain N space separated integers denoting the array elements.
-
-Output:
-Output the modified array with alternated elements.
 
 Constraints:
 1 <=T<= 100
 1 <=N<= 107
 1 <=arr[i]<= 107
+
+Input:
+First line of input conatins number of test cases T. 
+First line of test case contain an integer denoting the array size N and 
+second line of test case contain N space separated integers denoting the array elements.
+
+Output:
+Output the modified array with alternated elements.
+
+
 
 Example:
 Input:
@@ -28,4 +34,10 @@ Output:
 
 Explanation:
 Testcase 1: Max element = 6, min = 1, second max = 5, second min = 2, and so on... Modified array is : 6 1 5 2 4 3.
+Input:
+1
+4
+1 5 4 3
+Output:
+5 1 4 3 
 """
