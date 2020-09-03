30a4bc4a27b3fd198dbfba15862518e5dcd03843
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-03 16:00:53 +0530

    Update check subsequence.py

--- check subsequence.py
+++ check subsequence.py
@@ -1,14 +1,32 @@
 """
-Given two strings str1 and str2, find if str1 is a subsequence of str2. A subsequence is a sequence that can be derived from another sequence by deleting some elements without changing the order of the remaining elements (source: wiki). Expected time complexity is linear.
+Given two strings str1 and str2, find if str1 is a subsequence of str2.
+
+A subsequence is a sequence that can be derived from another sequence by deleting some elements without changing the order of the remaining elements.
+
+Expected time complexity is linear.
 
 Examples :
 
 Input: str1 = "AXY", str2 = "ADXCPY"
-Output: True (str1 is a subsequence of str2)
+Output: Yes (str1 is a subsequence of str2)
 
 Input: str1 = "AXY", str2 = "YADXCP"
-Output: False (str1 is not a subsequence of str2)
+Output: No (str1 is not a subsequence of str2)
 
 Input: str1 = "gksrek", str2 = "geeksforgeeks"
-Output: True (str1 is a subsequence of str2)
+Output: Yes (str1 is a subsequence of str2)
 """
+def isSubSequence(string1, string2,m,n):
+    if m == 0:    return True
+    if n == 0:    return False
+    if string1[m-1] == string2[n-1]: 
+        return isSubSequence(string1, string2, m-1, n-1)
+    return isSubSequence(string1, string2, m, n-1) 
+ 
+string1 = input()
+string2 = input()
+  
+if isSubSequence(string1, string2, len(string1),len(string2)): 
+    print("Yes")
+else: 
+    print("No")
