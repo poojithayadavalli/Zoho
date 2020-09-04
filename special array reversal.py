67da36fe2d27e740c904e3eabb857dd926ef954e
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-04 13:33:36 +0530

    Create special array reversal.py

---
+++ special array reversal.py
@@ -0,0 +1,34 @@
+"""
+Given a string S, containing special characters and all the alphabets, reverse the string without
+affecting the positions of the special characters.
+ 
+
+Input
+The first line of input contains an integer T denoting the number of test cases. Then T test cases
+follow. 
+The first line of each test case contains a case-insensitive string S.
+
+
+Output
+Print out the reversed string without  affecting special characters.
+
+
+Constraints
+1 <= T <= 100
+0 <   S  <= 100
+
+ 
+
+Examples 
+
+Input
+3
+A&B
+abc%sld*
+dakA&*hA@#N
+
+Output
+B&A
+dls%cba*
+NAhA&*ka@#d
+"""
