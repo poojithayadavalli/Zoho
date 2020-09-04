513d0126cffec60adf576f0acedbd85b764b4c0e
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-04 13:47:35 +0530

    Create number to word.py

---
+++ number to word.py
@@ -0,0 +1,28 @@
+"""
+Given number into words. For example, if “1234” is given as input, output should be “one thousand two hundred and thirty four”
+
+Input:
+
+The first line of input contains an integer T denoting the number of test cases.
+The first line of each test case is N.
+
+Output:
+
+Print the number into words (in small letter).
+
+Constraints:
+
+1 ≤ T ≤ 100
+1 ≤ N ≤ 9999
+
+Example:
+
+Input:
+2
+2
+142
+
+Output:
+two
+one hundred and forty two
+"""
