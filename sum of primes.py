d5894c744f7e24cd7184f815e6728ec81826789e
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-04 13:13:47 +0530

    Create sum of primes.py

---
+++ sum of primes.py
@@ -0,0 +1,27 @@
+"""
+Your task is to calculate sum  of primes present as digits of given number N.
+
+Input:
+
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. The next T lines contains an integer N.
+
+Output:
+Print sum of primes in the digit
+
+Constraints:
+1 ≤ T ≤ 50
+2 ≤ N ≤ 50000
+
+
+Example:
+
+Input:
+
+2
+333
+686
+
+Output:
+9
+0
+"""
