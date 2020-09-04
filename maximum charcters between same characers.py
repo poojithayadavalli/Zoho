ddf62af5b2cc84b5e8aef949b9e590e25403250f
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-04 13:16:50 +0530

    Create maximum charcters between same characers.py

---
+++ maximum charcters between same characers.py
@@ -0,0 +1,22 @@
+"""
+Given a string containing lower and uppercase alphabets, the task is to find the maximum number of characters between any two same character in the string.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case contains a string S.
+
+Output:
+For each test case, print the maximum number in new line. If no character repeats, print "-1".
+
+Constraints:
+1<=T<=100
+1<=|string length|<=105
+
+Example:
+Input:
+2
+socks
+FoR
+Output:
+3
+-1
+"""
