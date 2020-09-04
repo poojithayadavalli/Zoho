5e54e6cd7bc22c6a13d7fed811380d7525e76cc2
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-04 13:20:27 +0530

    Create is square.py

---
+++ is square.py
@@ -0,0 +1,20 @@
+"""
+Given four different points in space. Find whether these points can form a square or not.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases.The first line of each testcase contains x1, y1, x2, y2, x3, y3, x4, y4(four points coordinates) separated by space.
+
+Output:
+Print "Yes"(without quotes) if it is square else "No"(without quotes).
+
+Constraints:
+1 ≤ T ≤ 30
+1 ≤ x1, x2, x3, x4, y1, y2, y3, y4 ≤ 100
+
+Example:
+Input:
+1
+20 10 10 20 20 20 10 10
+Output:
+Yes
+"""
