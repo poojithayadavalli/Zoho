8b911ba6d6219aa9c9baf6e43c85b4c2762292bd
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-08 11:45:22 +0530

    Update sort in specific order.py

--- sort in specific order.py
+++ sort in specific order.py
@@ -1,16 +1,22 @@
 """
-Given an array A of positive integers. Your task is to sort them in such a way that the first part of the array contains odd numbers sorted in descending order, rest portion contains even numbers sorted in ascending order.
+Given an array A of positive integers. Your task is to sort them in such a way that the first part of the array contains odd numbers 
 
-Input:
-The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case contains an integer N denoting the size of the array. The next line contains N space separated values of the array.
-
-Output:
-For each test case in a new line print the space separated values of the modified array.
+sorted in descending order, rest portion contains even numbers sorted in ascending order.
 
 Constraints:
 1 <= T <= 103
 1 <= N <= 107
 0 <= Ai <= 1018
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. 
+Then T test cases follow. Each test case contains an integer N denoting the size of the array. 
+The next line contains N space separated values of the array.
+
+Output:
+For each test case in a new line print the space separated values of the modified array.
+
+
 
 Example:
 Input:
@@ -27,3 +33,17 @@ Output:
 Explanation:
 Testcase1: Array elements 7 5 3 1 are odd and sorted in descending order, whereas 2 4 10 are even numbers and sorted in ascending order.
 """
+t=int(input())
+while t>0:
+    n=int(input())
+    a=[int(x) for x in input().split()]
+    odd=[x for x in a if x%2!=0]
+    even=[x for x in a if x%2==0]
+    odd.sort(reverse=True)
+    even.sort()
+    for x in odd:
+        print(x,end=' ')
+    for x in even :
+        print(x,end=' ')
+    print()
+    t-=1
