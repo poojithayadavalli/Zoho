f42af4c226dc293fabc514f12839a8c9618b9c4d
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-08 14:09:40 +0530

    Update LRU cache.py

--- LRU cache.py
+++ LRU cache.py
@@ -4,6 +4,11 @@ get(x)   : Returns the value of the key x if the key exists in the cache otherwi
 set(x,y) : inserts the value if the key x is not already present. If the cache reaches its capacity it should invalidate the least recently used item before inserting the new item.
 In the constructor of the class the size of the cache should be intitialized.
 
+Constraints:
+1 <= N <= 1000
+1 <= Q <= 100000
+1 <= x, y <= 1000
+
 Example 1:
 
 Input:
@@ -45,125 +50,50 @@ You only need to complete the provided functions get() and set().
 Expected Time Complexity: O(1) for both get() and set().
 Expected Auxiliary Space: O(1) for both get() and set(). (though, you may use extra space for cache storage and implementation purposes).
 
-Constraints:
-1 <= N <= 1000
-1 <= Q <= 100000
-1 <= x, y <= 1000
+Input:
+2
+3
+PUT 1 2
+PUT 2 3
+Get 2
+Output:
+3
+Input:
+2
+3
+PUT 1 2
+PUT 1 3
+Get 1
+Output:
+3
 """
-import java.util.*;
-import java.lang.*;
+from collections import OrderedDict
+class LRUCache:
+    def __init__(self, capacity): 
+        self.cache = OrderedDict() 
+        self.capacity = capacity  
+    def get(self, key): 
+        if key not in self.cache: 
+            return "-1"
+        else: 
+            self.cache.move_to_end(key) 
+            return self.cache[key]
+    def put(self, key, value): 
+        self.cache[key] = value 
+        self.cache.move_to_end(key) 
+        if len(self.cache) > self.capacity: 
+            self.cache.popitem(last = False) 
+  
+n=int(input())
+cache = LRUCache(n)
+q=int(input())
+l=[]
+for i in range(q):
+    k=input().split()
+    if k[0]=="PUT":
+        cache.put(k[1],k[2])
+    else:
+        x=cache.get(k[1])
+        l.append(x)
+print("".join(l))
 
-  public class LRUDesign {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-
-            int capacity = Integer.parseInt(read.readLine());
-            int queries = Integer.parseInt(read.readLine());
-            LRUCache cache = new LRUCache(capacity);
-            String str[] = read.readLine().trim().split(" ");
-            int len = str.length;
-            int itr = 0;
-
-            for (int i = 0; (i < queries) && (itr < len); i++) {
-                String queryType = str[itr++];
-                if (queryType.equals("SET")) {
-                    int key = Integer.parseInt(str[itr++]);
-                    int value = Integer.parseInt(str[itr++]);
-                    cache.set(key, value);
-                }
-                if (queryType.equals("GET")) {
-                    int key = Integer.parseInt(str[itr++]);
-                    System.out.print(cache.get(key) + " ");
-                }
-            }
-            System.out.println();
-        }
-    }
-}
-// } Driver Code Ends
-
-
-
-// design the class in the most optimal way
-class Node{
-    int key,value;
-    Node next,prev;
-    Node(int key,int value){
-        this.key=key;
-        this.value=value;
-    }
-}
-class LRUCache
-{
-    private static HashMap<Integer,Node> hm;
-    private static Node head,tail;
-    private static int capacity,count;
-    LRUCache(int cap)
-    {
-        hm=new HashMap<>();
-        head=new Node(0,0);
-        tail=new Node(0,0);
-        capacity=cap;
-        count=0;
-        head.next=tail;
-        tail.prev=head;
-        head.prev=null;
-        tail.next=null;
-    }
-
-    // This method works in O(1)
-    public static int get(int key)
-    {
-        if(hm.get(key)!=null){
-            Node node=hm.get(key);
-            int result=node.value;
-            deleteNode(node);
-            addtohead(node);
-            return result;
-        }
-        return -1;
-    }
-    
-    public static void deleteNode(Node node){
-        node.prev.next=node.next;
-        node.next.prev=node.prev;
-    }
-    
-    public static void addtohead(Node node){
-        node.next=head.next;
-        node.next.prev=node;
-        head.next=node;
-        node.prev=head;
-    }
-
-    // This method works in O(1)
-    public static void set(int key, int value)
-    {
-        if(hm.get(key)!=null){
-            Node node=hm.get(key);
-            node.value=value;
-            hm.put(key,node);
-            deleteNode(node);
-            addtohead(node);
-        }
-        else{
-            Node node=new Node(key,value);
-            hm.put(key,node);
-            if(count<capacity){
-                addtohead(node);
-                count++;
-            }
-            else{
-                hm.remove(tail.prev.key);
-                deleteNode(tail.prev);
-                addtohead(node);
-            }
-        }
-    }
-}
