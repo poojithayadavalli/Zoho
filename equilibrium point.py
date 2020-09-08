91fa5c23f2f01e58e3522d2fe4fab690da56de83
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-08 15:27:16 +0530

    Update equilibrium point.py

--- equilibrium point.py
+++ equilibrium point.py
@@ -1,8 +1,12 @@
 """
-Given an array A of N positive numbers. The task is to find the position where equilibrium first occurs in the array. Equilibrium position in an array is a position such that the sum of elements before it is equal to the sum of elements after it.
+Given an array A of N positive numbers. The task is to find the position where equilibrium first occurs in the array.
+
+Equilibrium position in an array is a position such that the sum of elements before it is equal to the sum of elements after it.
 
 Input:
-The first line of input contains an integer T, denoting the number of test cases. Then T test cases follow. First line of each test case contains an integer N denoting the size of the array. Then in the next line are N space separated values of the array A.
+The first line of input contains an integer T, denoting the number of test cases. Then T test cases follow.
+First line of each test case contains an integer N denoting the size of the array. 
+Then in the next line are N space separated values of the array A.
 
 Output:
 For each test case in a new  line print the position at which the elements are at equilibrium if no equilibrium point exists print -1.
@@ -28,3 +32,21 @@ Explanation:
 Testcase 1: Since its the only element hence its the only equilibrium point.
 Testcase 2: For second test case equilibrium point is at position 3 as elements below it (1+3) = elements after it (2+2).
 """
+T= int(input())
+for i in range(T):
+    N=int(input())
+    A=list(map(int,input().split()))
+    s=sum(A)
+    r=s-A[0]
+    l=0
+    k=-1
+    if(r==l):
+        print(1)
+        continue
+    for i in range(1,N):
+        if(r==l):
+            k=i
+            break
+        r=r-A[i]
+        l=l+A[i-1]
+    print(k)    
