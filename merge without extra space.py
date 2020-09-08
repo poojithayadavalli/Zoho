63ad7837c87af6cb5af013639b22ecb03a05aa9d
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-08 13:23:06 +0530

    Update merge without extra space.py

--- merge without extra space.py
+++ merge without extra space.py
@@ -1,7 +1,14 @@
 """
-Given two sorted arrays arr1[] and arr2[] in non-decreasing order with size n and m. The task is to merge the two sorted arrays into one sorted array (in non-decreasing order).
+Given two sorted arrays arr1[] and arr2[] in non-decreasing order with size n and m.
 
-Note: Expected time complexity is O((n+m) log(n+m)). DO NOT use extra space.  We need to modify existing arrays as following.
+The task is to merge the two sorted arrays into one sorted array (in non-decreasing order).
+
+Note: Expected time complexity is O((n+m) log(n+m)).
+
+Constraints:
+1 <= T <= 100
+1 <= X, Y <= 5*104
+0 <= arr1i, arr2i <= 109
 
 Input: arr1[] = {10};
        arr2[] = {2, 3};
@@ -13,15 +20,14 @@ Input: arr1[] = {1, 5, 9, 10, 15, 20};
 Output: arr1[] = {1, 2, 3, 5, 8, 9}
         arr2[] = {10, 13, 15, 20} 
 Input:
-First line contains an integer T, denoting the number of test cases. First line of each test case contains two space separated integers X and Y, denoting the size of the two sorted arrays. Second line of each test case contains X space separated integers, denoting the first sorted array P. Third line of each test case contains Y space separated integers, denoting the second array Q.
+First line contains an integer T, denoting the number of test cases.
+First line of each test case contains two space separated integers X and Y, denoting the size of the two sorted arrays.
+Second line of each test case contains X space separated integers, denoting the first sorted array P. 
+Third line of each test case contains Y space separated integers, denoting the second array Q.
 
 Output:
 For each test case, print (X + Y) space separated integer representing the merged array.
 
-Constraints:
-1 <= T <= 100
-1 <= X, Y <= 5*104
-0 <= arr1i, arr2i <= 109
 
 Example:
 Input:
@@ -40,3 +46,13 @@ Output:
 Explanation:
 Testcase 1: After merging two non-decreasing arrays, we have, 0 1 2 3 5 6 7 8 9.
 """
+t=int(input())
+while t>0:
+    m , n = map(int, input().split())
+    a1=list(map(int, input().split()))
+    a2=list(map(int, input().split()))
+    c=a1+a2
+    c.sort()
+    print(*c)
+    
+    t=t-1
