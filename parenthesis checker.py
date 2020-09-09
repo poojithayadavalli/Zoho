e3fdbc5fa7bc1560f49ef06eaa34aacd31b7afba
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-09 13:41:16 +0530

    Update parenthesis checker.py

--- parenthesis checker.py
+++ parenthesis checker.py
@@ -24,3 +24,22 @@ balanced
 balanced
 not balanced
 """
+def cal(s):
+    stack = []
+    ans = "balanced"
+    dic = {'(' : ')', '{' : '}', '[' : ']'}
+    for i in s:
+        if i == '(' or i == '{' or i == '[':
+            stack.append(i)
+        elif not stack or dic[stack.pop()] != i:
+            ans = "not balanced"
+            break
+    if stack:
+        ans = "not balanced"
+    return ans
+
+
+for _ in range(int(input())):
+    s = input()
+    a = cal(s)
+    print(a)
