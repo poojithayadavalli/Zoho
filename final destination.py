690dc58559e31950190c064f4e5c8400ba0630d4
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-09 16:07:06 +0530

    Update final destination.py

--- final destination.py
+++ final destination.py
@@ -1,5 +1,7 @@
 """
-Consider a 2d plane and  a Robot which is located at (0,0) who can move only one unit step at a time in any direction  ie if its initial position is (x,y)  he can go to positions (x + 1, y), (x - 1, y), (x, y + 1) or (x, y - 1). Now Given three integers a,b (denoting the final position where the robot has
+Consider a 2d plane and  a Robot which is located at (0,0) who can move only one unit step at a time in any direction  
+ie if its initial position is (x,y)  he can go to positions (x + 1, y), (x - 1, y), (x, y + 1) or (x, y - 1). 
+Now Given three integers a,b (denoting the final position where the robot has
  to reach)  and x (denoting the steps in which he has to reach).
 
 Input:
@@ -24,3 +26,17 @@ Output:
 0
 1
 """
+def reach(a,b,steps):
+    a=abs(a)
+    b = abs(b)
+    if (a+b)>steps:
+        print(0)
+    elif (steps-(a+b))%2==0:
+        print(1)
+    else:
+        print(0)
+t=int(input())
+for _ in range(t):
+    a,b,s=map(int,input().split())
+    reach(a,b,s)
+        
