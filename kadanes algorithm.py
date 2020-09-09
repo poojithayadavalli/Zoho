0d65d8bb3cc82b6942d68fe901d6cbfa653ea39a
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-09-09 12:34:49 +0530

    Update kadanes algorithm.py

--- kadanes algorithm.py
+++ kadanes algorithm.py
@@ -2,7 +2,9 @@
 Given an array arr of N integers. Find the contiguous sub-array with maximum sum.
 
 Input:
-The first line of input contains an integer T denoting the number of test cases. The description of T test cases follows. The first line of each test case contains a single integer N denoting the size of array. The second line contains N space-separated integers A1, A2, ..., AN denoting the elements of the array.
+The first line of input contains an integer T denoting the number of test cases. 
+The description of T test cases follows. The first line of each test case contains a single integer N denoting the size of array.
+The second line contains N space-separated integers A1, A2, ..., AN denoting the elements of the array.
 
 Output:
 Print the maximum sum of the contiguous sub-array in a separate line for each test case.
@@ -26,3 +28,15 @@ Output
 Explanation:
 Testcase 1: Max subarray sum is 9 of elements (1, 2, 3, -2, 5) which is a contiguous subarray.
 """
+t=int(input())
+while t>0:
+    n = int(input())
+    a = list(map(int, input().split()))
+    max_so_far =a[0] 
+    curr_max = a[0]  
+    for i in range(1,len(a)): 
+        curr_max = max(a[i], curr_max + a[i]) 
+        max_so_far = max(max_so_far,curr_max) 
+          
+    print(max_so_far) 
+    t=t-1
